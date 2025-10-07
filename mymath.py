8e6f29b2db6cf244baf1cfa0ed6e0aeab79684ac
LAPTOP-VUP90BJA\Zephyrus <joe0511ip@gmail.com>
2025-10-07 10:18:09 +0800

    plus

--- mymath.py
+++ mymath.py
@@ -1,3 +1,6 @@
+def add(a, b):
+    return a + b
+
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 choice = input("Enter choice (+,-,*,/): ")
@@ -12,5 +15,3 @@ elif choice == '/':
     print(f"Result: {divide(num1, num2)}")
 else:
     print("Invalid input")
-def add(a, b):
-    return a + b
